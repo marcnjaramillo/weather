5094332502e4861206703cd4875fdd3b9fd662c8
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-06-04 23:41:47 -0700

    Update App with new SearchInput component

--- App.js
+++ App.js
@@ -1,19 +1,129 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default function App() {
+import getImageForWeather from './utils/getImageForWeather';
+
+import SearchInput from './components/SearchInput';
+
+// class App extends Component {
+
+//   state = {
+//     location: 'San Fransisco'
+//   }
+
+//   handleUpdateLocation = city => {
+//     this.setState({
+//       location: city,
+//     });
+//   };
+
+//   render() {
+//     const { location } = this.state;
+
+//     return (
+//       <KeyboardAvoidingView
+//         style={styles.container}
+//         behavior='height'
+//       >
+//         <ImageBackground
+//           source={getImageForWeather('Clear')}
+//           style={styles.imageContainer}
+//           imageStyle={styles.image}
+//         >
+//           <View style={styles.detailsContainer}>
+//             <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+//             <Text style={[styles.smallText, styles.textStyle]}>Slightly cloudy</Text>
+//             <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+//             <SearchInput
+//               placeholder='Search any city'
+//               onSubmit={this.handleUpdateLocation}
+//             />
+//           </View>
+
+//         </ImageBackground>
+//       </KeyboardAvoidingView>
+//     );
+//   }
+// }
+
+const App = () => {
+
+  const [location, setLocation] = useState('San Fransisco');
+
+  const handleUpdateLocation = city => {
+    setLocation(city);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior='height'
+    >
+      <ImageBackground
+        source={getImageForWeather('Clear')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View style={styles.detailsContainer}>
+          <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+          <Text style={[styles.smallText, styles.textStyle]}>Slightly cloudy</Text>
+          <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+          <SearchInput
+            placeholder='Search any city'
+            onSubmit={handleUpdateLocation}
+          />
+        </View>
+
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+  },
+  textStyle: {
+    textAlign: 'center',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'AvenirNext-Regular',
+      },
+      android: {
+        fontFamily: 'Roboto',
+      },
+    }),
+    color: 'white',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
   },
 });
+
+export default App;
